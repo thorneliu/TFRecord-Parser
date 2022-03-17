5c5d1a42417a51ffb13d975f78ea9927bf46bbd0
LiuQiang <thorneliu@gmail.com>
2022-03-17 15:57:32 +0800

    Update parse_tfrecord.cpp

--- parse_tfrecord.cpp
+++ parse_tfrecord.cpp
@@ -6,112 +6,81 @@
 #include "example.pb.h"
 using namespace std;
 
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <streambuf>
+#include "example.pb.h"
+#include "keys.h"
+#include <vector>
+using namespace std;
+
 // Iterates though all feature in the example and print them.
 void ListEntry(const tensorflow::Example& example) {
     auto features = example.features();
-    int feature_size = features.feature_size(); 
-    cout << "total feature number: " << features.feature_size() << endl;
-    
+    ostringstream oss;
+
     // all feature is const ::PROTOBUF_NAMESPACE_ID::Map<std::string, ::tensorflow::Feature>
-    auto all_feature_map = features.feature();
-    for (google::protobuf::Map< std::string, tensorflow::Feature >::const_iterator it = all_feature_map.begin(); it != all_feature_map.end(); ++it) {
-        cout << "---------------------------" << endl;
-        auto cur_feature_name = it->first;
-        cout << cur_feature_name << endl;
-        auto cur_feature_value = it->second; // is class tensorflow::Feature
+    auto& all_feature_map = features.feature();
+    for (auto& key : vecKeys)
+    {
+        if (0 == all_feature_map.count(key)) continue;
+        auto& cur_feature_value = all_feature_map.at(key);
 
-        // print bytes list
         if(cur_feature_value.has_bytes_list()) {
-            cout << "all bytes values: " << endl;
-            auto bytes_list = cur_feature_value.bytes_list();
-            for (int i = 0; i < bytes_list.value_size(); ++i)
-                cout << bytes_list.value(i) << ", " ;
-            cout << endl;
+            auto& bytes_list = cur_feature_value.bytes_list();
+            oss << bytes_list.value_size() << " ";
+            for (auto& val : bytes_list.value())
+            {
+                oss << val << " ";
+            }
         }
 
-        // print int64 list
         if(cur_feature_value.has_int64_list()) {
-            cout << "all int64 values: " << endl;
-            auto int64_list = cur_feature_value.int64_list();
-            for (int i = 0; i < int64_list.value_size(); ++i)
-                cout << int64_list.value(i) << ", " ;
-            cout << endl;
+            auto& int64_list = cur_feature_value.int64_list();
+            oss << int64_list.value_size() << " ";
+            for (auto& val : int64_list.value())
+            {
+                oss << val << " ";
+            }
         }
 
-        // print float list
         if(cur_feature_value.has_float_list()) {
-            cout << "all float values: " << endl;
-            auto float_list = cur_feature_value.float_list();
-            for (int i = 0; i < float_list.value_size(); ++i)
-                cout << float_list.value(i) << ", " ;
-            cout << endl;
+            auto& float_list = cur_feature_value.float_list();
+            oss << float_list.value_size() << " ";
+            for (auto& val : float_list.value())
+            {
+                oss << val << " ";
+            }
         }
     }
-}
 
-struct membuf : std::streambuf
-{
-    membuf(char* begin, char* end) {
-        this->setg(begin, begin, end);
-    }
-};
-
-void print_hex(char* buf, int len) {
-    buf[len] = '\0';
-    for(int i = 0; i < len; ++i) {
-        cout << setfill('0') << setw(2) << hex << int(buf[i]) << " " ;
-        if(i && (i % 10 == 0))
-            cout << endl;
-    }
-    cout << endl;
+    cout << oss.str() << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-
-    if (argc != 2) {
-        cerr << "Usage:  " << argv[0] << " FILE_PATH" << endl;
-        return -1;
-    }
-
-    // Read the file.
-    fstream input(argv[1], ios::in | ios::binary);
-
-    // repeatedly Read length
+    char buffer[50000];
     uint64_t length = 0;
-    uint64_t max_len = 1000;
-    char* data_buffer = new char[max_len];
-    
-    while(input.read(reinterpret_cast<char *>(&length), 8)) {
-        if(length > max_len) {
-            delete data_buffer;
-            max_len = 2 * length;
-            data_buffer = new char[max_len];
+    int crc1 = 0, crc2 = 0;
+    int i = 0;
+    tensorflow::Example example;
+    while (cin)
+    {
+        cin.read((char *)(&length), sizeof(length));
+        if (!cin) break;
+        //cin.read((char*)(&crc1), sizeof(crc1));
+        //if (!cin) break;
+        cin.read(buffer, length + 8);
+        //if (!cin) break;
+        //cin.read((char*)(&crc2), sizeof(crc2));
+
+        if (example.ParseFromArray((void*)(buffer + 4), length)) {
+            ListEntry(example);
         }
-        cout << "=======================" << endl;
-        cout << "current example length: " << length << endl;
-
-        // the following fields: crc of length(4 bytes), data(length bytes), crc of data(4 bytes)
-        // we directly read in (length+8) bytes, and skip the head/tail
-        input.read(data_buffer, length + 8);
-
-        /*
-        print_hex(data_buffer, 4); // print crc of length
-        print_hex(data_buffer + 4, length); // print data
-        print_hex(data_buffer + length + 4, 4); // print crc of data
-        */
-        
-        tensorflow::Example example;
-        if (!example.ParseFromArray(reinterpret_cast<void*>(data_buffer + 4), length)) {
-            cerr << "Failed to parse file." << endl;
-            return -1;
-        } else {
-            cout << "successfully parse file into an Example" << endl;
-        }
-
-        ListEntry(example);
     }
 
-    input.close();
     google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
